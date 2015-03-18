95d2d727422b280a995e94a22736ea12fd2a1baf
Markus Metzger <markus.t.metzger@intel.com>
2015-03-18 10:37:52 +0100

    libipt: map files on windows
    Use CreateFileMapping() and MapViewOffile() to map file sections into memory
    on Windows.  Fall back to FILE-based sections if the former fails.
    
    Change-Id: I9d32708d5a631292725f2b7b010525e1eb9fac24
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>

--- libipt/src/windows/pt_section_windows.c
+++ libipt/src/windows/pt_section_windows.c
@@ -27,66 +27,171 @@
  */
 
 #include "pt_section.h"
+#include "pt_section_windows.h"
 #include "pt_section_file.h"
 
 #include "intel-pt.h"
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
 
 
-static uint64_t file_size(const char *filename)
+static int pt_sec_windows_fstat(const char *filename, struct _stat *stat)
 {
-	FILE *file;
-	long fsize;
-	int errcode;
+	int fd, errcode;
 
-	if (!filename)
-		return 0ull;
+	if (!filename || !stat)
+		return -pte_internal;
 
-	file = fopen(filename, "rb");
-	if (!file)
-		return 0ull;
+	fd = _open(filename, _O_RDONLY);
+	if (fd == -1)
+		return -pte_bad_image;
 
-	/* Determine the size of the file. */
-	errcode = fseek(file, 0, SEEK_END);
+	errcode = _fstat(fd, stat);
+
+	_close(fd);
+
 	if (errcode)
-		return 0ull;
+		return -pte_bad_image;
 
-	fsize = ftell(file);
-	if (fsize < 0)
-		return 0ull;
-
-	return (uint64_t) fsize;
+	return 0;
 }
 
 int pt_section_mk_status(void **pstatus, uint64_t *psize, const char *filename)
 {
-	uint64_t size, *status;
+	struct pt_sec_windows_status *status;
+	struct _stat stat;
+	int errcode;
 
-	if (!pstatus || !psize || !filename)
+	if (!pstatus || !psize)
 		return -pte_internal;
 
-	size = file_size(filename);
-	if (!size)
+	errcode = pt_sec_windows_fstat(filename, &stat);
+	if (errcode < 0)
+		return errcode;
+
+	if (stat.st_size < 0)
 		return -pte_bad_image;
 
-	status = malloc(sizeof(size));
+	status = malloc(sizeof(*status));
 	if (!status)
 		return -pte_nomem;
 
-	*status = size;
+	status->stat = stat;
+
 	*pstatus = status;
-	*psize = size;
+	*psize = stat.st_size;
 
 	return 0;
+}
+
+static int check_file_status(struct pt_section *section, int fd)
+{
+	struct pt_sec_windows_status *status;
+	struct _stat stat;
+	int errcode;
+
+	if (!section)
+		return -pte_internal;
+
+	errcode = _fstat(fd, &stat);
+	if (errcode)
+		return -pte_bad_image;
+
+	status = section->status;
+	if (!status)
+		return -pte_internal;
+
+	if (stat.st_size != status->stat.st_size)
+		return -pte_bad_image;
+
+	if (stat.st_mtime != status->stat.st_mtime)
+		return -pte_bad_image;
+
+	return 0;
+}
+
+static DWORD granularity(void)
+{
+	struct _SYSTEM_INFO sysinfo;
+
+	GetSystemInfo(&sysinfo);
+
+	return sysinfo.dwAllocationGranularity;
+}
+
+int pt_sec_windows_map(struct pt_section *section, int fd)
+{
+	struct pt_sec_windows_mapping *mapping;
+	uint64_t offset, size, adjustment;
+	HANDLE fh, mh;
+	DWORD dsize;
+	uint8_t *base;
+
+	if (!section)
+		return -pte_internal;
+
+	offset = section->offset;
+	size = section->size;
+
+	adjustment = offset % granularity();
+
+	offset -= adjustment;
+	size += adjustment;
+
+	/* The section is supposed to fit into the file so we shouldn't
+	 * see any overflows, here.
+	 */
+	if (size < section->size)
+		return -pte_internal;
+
+	dsize = (DWORD) size;
+	if ((uint64_t) dsize != size)
+		return -pte_internal;
+
+	fh = (HANDLE) _get_osfhandle(fd);
+
+	mh = CreateFileMapping(fh, NULL, PAGE_READONLY, 0, 0, NULL);
+	if (!mh)
+		return -pte_bad_image;
+
+	base = MapViewOfFile(mh, FILE_MAP_READ, (DWORD) (offset >> 32),
+			     (DWORD) (uint32_t) offset, dsize);
+	if (!base)
+		goto out_mh;
+
+	mapping = malloc(sizeof(*mapping));
+	if (!mapping)
+		goto out_map;
+
+	mapping->fd = fd;
+	mapping->mh = mh;
+	mapping->base = base;
+	mapping->begin = base + adjustment;
+	mapping->end = base + size;
+
+	section->mapping = mapping;
+	section->unmap = pt_sec_windows_unmap;
+	section->read = pt_sec_windows_read;
+
+	return 0;
+
+
+out_map:
+	UnmapViewOfFile(base);
+
+out_mh:
+	CloseHandle(mh);
+	return -pte_bad_image;
 }
 
 int pt_section_map(struct pt_section *section)
 {
 	const char *filename;
-	uint64_t size, *status;
+	HANDLE fh;
 	FILE *file;
-	int errcode;
+	int fd, errcode;
 
 	if (!section || section->mapping)
 		return -pte_internal;
@@ -95,18 +200,47 @@ int pt_section_map(struct pt_section *section)
 	if (!filename)
 		return -pte_internal;
 
-	status = section->status;
-	if (!status)
-		return -pte_internal;
+	fh = CreateFile(filename, GENERIC_READ, FILE_SHARE_READ, NULL,
+			OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (fh == INVALID_HANDLE_VALUE) {
+		/* We failed to open the file read-only.  Let's try to open it
+		 * read-write; maybe our user has the file open for writing.
+		 *
+		 * We will detect changes to the file via fstat().
+		 */
 
-	file = fopen(filename, "rb+");
-	if (!file)
-		return -pte_bad_image;
+		fh = CreateFile(filename, GENERIC_READ, FILE_SHARE_WRITE, NULL,
+				OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (fh == INVALID_HANDLE_VALUE)
+			return -pte_bad_image;
+	}
 
-	size = file_size(filename);
-	if (size != *status) {
+	fd = _open_osfhandle((intptr_t) fh, _O_RDONLY);
+	if (fd == -1) {
 		errcode = -pte_bad_image;
-		goto err_file;
+		goto out_fh;
+	}
+
+	errcode = check_file_status(section, fd);
+	if (errcode < 0) {
+		errcode = -pte_bad_image;
+		goto out_fd;
+	}
+
+	/* We leave the file open on success.  It will be closed when the
+	 * section is unmapped.
+	 */
+	errcode = pt_sec_windows_map(section, fd);
+	if (!errcode)
+		return 0;
+
+	/* Fall back to file based sections - report the original error
+	 * if we fail to convert the file descriptor.
+	 */
+	file = _fdopen(fd, "rb");
+	if (!file) {
+		errcode = -pte_bad_image;
+		goto out_fd;
 	}
 
 	errcode = pt_sec_file_map(section, file);
@@ -121,4 +255,70 @@ int pt_section_map(struct pt_section *section)
 err_file:
 	fclose(file);
 	return errcode;
+
+out_fd:
+	_close(fd);
+	return errcode;
+
+out_fh:
+	CloseHandle(fh);
+	return errcode;
+}
+
+int pt_sec_windows_unmap(struct pt_section *section)
+{
+	struct pt_sec_windows_mapping *mapping;
+
+	if (!section)
+		return -pte_internal;
+
+	mapping = section->mapping;
+	if (!mapping || !section->unmap || !section->read)
+		return -pte_internal;
+
+	section->mapping = NULL;
+	section->unmap = NULL;
+	section->read = NULL;
+
+	UnmapViewOfFile(mapping->begin);
+	CloseHandle(mapping->mh);
+	_close(mapping->fd);
+	free(mapping);
+
+	return 0;
+}
+
+int pt_sec_windows_read(const struct pt_section *section, uint8_t *buffer,
+		      uint16_t size, uint64_t offset)
+{
+	struct pt_sec_windows_mapping *mapping;
+	const uint8_t *begin, *end;
+	int bytes;
+
+	if (!buffer || !section)
+		return -pte_invalid;
+
+	mapping = section->mapping;
+	if (!mapping)
+		return -pte_internal;
+
+	begin = mapping->begin + offset;
+	end = begin + size;
+
+	if (end < begin)
+		return -pte_nomap;
+
+	if (mapping->end <= begin)
+		return -pte_nomap;
+
+	if (begin < mapping->begin)
+		return -pte_nomap;
+
+	if (mapping->end < end)
+		end = mapping->end;
+
+	bytes = (int) (end - begin);
+
+	memcpy(buffer, begin, bytes);
+	return bytes;
 }
