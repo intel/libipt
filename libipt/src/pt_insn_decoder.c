9070c90207cb91a9562f928cef4dcf5cd7a4bc57
Markus Metzger <markus.t.metzger@intel.com>
2017-07-24 10:33:11 +0200

    libipt: skl014
    SKL014: Intel(R) PT TIP.PGD May Not Have Target IP Payload.
    
            When Intel PT (Intel Processor Trace) is enabled and a direct
            unconditional branch clears IA32_RTIT_STATUS.FilterEn (MSR 571H,
            bit 0), due to this erratum, the resulting TIP.PGD (Target IP
            Packet, Packet Generation Disable) may not have an IP payload
            with the target IP.
    
    We extend the workaround to take the filter configuration into account.
    
    Note that we use the filter configuration solely to determine the correct direct
    unconditional branch at which to disable tracing.  The branch itself may already
    lie outside of the filter region.
    
    This fixes #21.
    
    Change-Id: I946522c94e91d257723d85477d8436202dcee22a
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>

--- libipt/src/pt_insn_decoder.c
+++ libipt/src/pt_insn_decoder.c
@@ -612,20 +612,52 @@ static int pt_insn_proceed(struct pt_insn_decoder *decoder,
 	return 0;
 }
 
-static int pt_insn_skl014(const struct pt_insn *insn,
-			  const struct pt_insn_ext *iext)
+static int pt_insn_at_skl014(const struct pt_event *ev,
+			     const struct pt_insn *insn,
+			     const struct pt_insn_ext *iext,
+			     const struct pt_config *config)
 {
-	if (!insn || !iext)
+	uint64_t ip;
+	int status;
+
+	if (!ev || !insn || !iext || !config)
+		return -pte_internal;
+
+	if (!ev->ip_suppressed)
 		return 0;
 
 	switch (insn->iclass) {
-	default:
-		return 0;
-
 	case ptic_call:
 	case ptic_jump:
-		return iext->variant.branch.is_direct;
+		/* The erratum only applies to unconditional direct branches. */
+		if (!iext->variant.branch.is_direct)
+			break;
+
+		/* Without a filter configuration, we bind the event to the
+		 * first matching branch.
+		 */
+		if (!config->addr_filter.config.addr_cfg)
+			return 1;
+
+		/* Otherwise we check the filter against the branch target. */
+		ip = insn->ip;
+		ip += insn->size;
+		ip += iext->variant.branch.displacement;
+
+		status = pt_filter_addr_check(&config->addr_filter, ip);
+		if (status <= 0) {
+			if (status < 0)
+				return status;
+
+			return 1;
+		}
+		break;
+
+	default:
+		break;
 	}
+
+	return 0;
 }
 
 static int pt_insn_at_disabled_event(const struct pt_event *ev,
@@ -642,9 +674,9 @@ static int pt_insn_at_disabled_event(const struct pt_event *ev,
 		    pt_insn_changes_cr3(insn, iext))
 			return 1;
 
-		if (config->errata.skl014 && pt_insn_skl014(insn, iext))
+		if (config->errata.skl014 &&
+		    pt_insn_at_skl014(ev, insn, iext, config))
 			return 1;
-
 	} else {
 		switch (insn->iclass) {
 		case ptic_ptwrite:
