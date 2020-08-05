87db4104cac9167a4b893d0445d4c7c636e84377
Markus Metzger <markus.t.metzger@intel.com>
2020-08-05 13:34:29 +0200

    libipt: support 6/143
    We do not know about the errata for those microarchitectures but let's
    assume that we carry forward existing errata until we know more.
    
    Change-Id: Ie1c147cfa91d3f0f323a5cceb97fd29735589941
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>

--- libipt/src/pt_config.c
+++ libipt/src/pt_config.c
@@ -74,6 +74,7 @@ int pt_cpu_errata(struct pt_errata *errata, const struct pt_cpu *cpu)
 		case 0x55:
 		case 0x6a:
 		case 0x6c:
+		case 0x8f:
 			errata->bdm70 = 1;
 			errata->skl014 = 1;
 			errata->skd022 = 1;
