0d364a142bd4823d01ccbf3173699d8385e8767a
Markus Metzger <markus.t.metzger@intel.com>
2022-09-14 15:21:56 +0200

    all: add SPDX license identifier
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>

--- script/perf-copy-mapped-files.bash
+++ script/perf-copy-mapped-files.bash
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
 # Copyright (c) 2015-2022, Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
