859e4865863e96feb8158fee40b2d8286c1affcf
Markus Metzger <markus.t.metzger@intel.com>
2022-10-04 06:27:11 +0200

    libipt: support 6/173 and 6/174
    We do not know about the errata for those microarchitectures but let's
    assume that we carry forward existing errata until we know more.
    
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>

--- libipt/src/pt_config.c
+++ libipt/src/pt_config.c
@@ -75,6 +75,8 @@ int pt_cpu_errata(struct pt_errata *errata, const struct pt_cpu *cpu)
 		case 0x6a:
 		case 0x6c:
 		case 0x8f:
+		case 0xad:
+		case 0xae:
 		case 0x66:
 		case 0x7d:
 		case 0x7e:
